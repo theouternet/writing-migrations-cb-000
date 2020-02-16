f504a0a6df72784dabaed7bf126d266d8ec7edc5
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-02-16 19:21:04 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-Class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
 def change 
   change_column :students, :birthdate, :datetime
 end 
